579e46ee169a7b9108fe82df800757af0b137089
Kinisjuniors <134644001+Kinisjuniors@users.noreply.github.com>
2025-10-17 16:07:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ OUTPUT_DIR = "output_files"
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.5-flash")
 TEMPERATURE = float(os.getenv("GEMINI_TEMPERATURE", "0.3"))
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyAV4M8FSw3ynZiMYm3HfS-A6g6mbrc6K5g")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyAV4M8FSw3ynZiMYm3Hrc6K5g")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 os.makedirs(INPUT_DIR, exist_ok=True)
